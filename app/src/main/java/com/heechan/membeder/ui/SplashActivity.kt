61e1d1346166416627aef727157903d3c1098e4e
Heechan <ckstmznf@naver.com>
2022-09-24 00:47:54 +0900

    add:Splash activity layout

---
+++ app/src/main/java/com/heechan/membeder/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.heechan.membeder.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heechan.membeder.R
+import com.heechan.membeder.base.BaseActivity
+import com.heechan.membeder.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
