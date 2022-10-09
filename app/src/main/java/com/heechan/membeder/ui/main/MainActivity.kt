d39cbb58f3225361d13a19c816eb7b692104169e
Heechan <ckstmznf@naver.com>
2022-10-10 02:52:37 +0900

    Dialog
    다이어로그의 width를 math_parent 적용은 아직 안함, 추후에 할 예정

--- app/src/main/java/com/heechan/membeder/ui/main/MainActivity.kt
+++ app/src/main/java/com/heechan/membeder/ui/main/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.heechan.membeder.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Toast
 import com.heechan.membeder.R
 import com.heechan.membeder.base.BaseActivity
 import com.heechan.membeder.databinding.ActivityMainBinding
+import com.heechan.membeder.ui.SplashActivity
+import com.heechan.membeder.view.AlertDialog
+import com.heechan.membeder.view.TagView
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.btnMainFill.setOnClickListener {
+            val okCallBack: () -> Unit = {
+                Toast.makeText(this, "확인 클릭", Toast.LENGTH_SHORT).show()
+            }
+
+            val dialog = AlertDialog(title = "저는 제목 입니다", message = "저는 설명문입니다.", okCallBack = okCallBack).apply {
+                show(supportFragmentManager, "alert dialog")
+            }
+        }
     }
 }
