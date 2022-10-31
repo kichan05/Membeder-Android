8b722ddef1d277d0a59457ecb47bcb4259451653
Heechan <ckstmznf@naver.com>
2022-11-01 01:54:41 +0900

    SignUpFragment prev

--- app/src/main/java/com/heechan/membeder/ui/signUp/SignUpFragment.kt
+++ app/src/main/java/com/heechan/membeder/ui/signUp/SignUpFragment.kt
@@ -26,4 +26,19 @@ abstract class SignUpFragment<D : ViewDataBinding>(
 
         findNavController().navigate(actionId)
     }
+
+    protected fun gotoPrev() {
+        val actionId = when (currentPage) {
+            2  -> R.id.action_signUp2Fragment_to_signUp1Fragment
+            3  -> R.id.action_signUp3Fragment_to_signUp2Fragment
+            4  -> R.id.action_signUp4Fragment_to_signUp3Fragment
+            5  -> R.id.action_signUp5Fragment_to_signUp4Fragment
+            6  -> R.id.action_signUp6Fragment_to_signUp5Fragment
+            7  -> R.id.action_signUp7Fragment_to_signUp6Fragment
+            8  -> R.id.action_signUp8Fragment_to_signUp7Fragment
+            else  -> throw Exception("Navigation page index overflow")
+        }
+
+        findNavController().navigate(actionId)
+    }
 }
