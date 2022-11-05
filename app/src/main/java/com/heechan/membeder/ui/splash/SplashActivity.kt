e543f17eb32531b10fb61b7ba3bb568cfa87b62e
Heechan <ckstmznf@naver.com>
2022-11-05 16:30:53 +0900

    Get GCP id token

--- app/src/main/java/com/heechan/membeder/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/heechan/membeder/ui/splash/SplashActivity.kt
@@ -8,7 +8,10 @@ import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.heechan.membeder.BuildConfig
 import com.heechan.membeder.R
 import com.heechan.membeder.base.BaseActivity
@@ -29,12 +32,12 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         }
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.btnSplashStart.setOnClickListener(gotoRegister)
         binding.txtSplashGotoLogin.setOnClickListener(gotoLogin)
+        binding.btnLoginGoogleLogin.setOnClickListener(googleLogin)
 
         viewModel.saveToken.observe(this) {
             Log.d("saveLoginData", it)
@@ -60,6 +63,24 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            GOOGLE_SIGN_IN -> {
+                val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    Log.d("googleLogin", "${account.idToken}")
+                }
+                catch (e : ApiException) {
+
+                }
+            }
+        }
+    }
+
     private fun gotoMain() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
@@ -71,12 +92,24 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         startActivity(intent)
     }
 
+    private val googleLogin : (View) -> Unit = {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(BuildConfig.OAUTH_GOOGLE_ID)
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN)
+    }
+
     private val gotoLogin: (View) -> Unit = {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 
     companion object {
-        const val GOOGLE_SIGN_IN = 312
+        const val GOOGLE_SIGN_IN = 312 //3월 12일은 내 생일~
     }
 }
