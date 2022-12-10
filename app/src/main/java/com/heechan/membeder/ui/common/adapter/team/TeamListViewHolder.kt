1b67878d4c4a5092cc7938387f11bf0ad0f0494b
Heechan <ckstmznf@naver.com>
2022-12-10 21:06:57 +0900

    Team List repackage

--- app/src/main/java/com/heechan/membeder/ui/common/adapter/team/TeamListViewHolder.kt
+++ app/src/main/java/com/heechan/membeder/ui/common/adapter/team/TeamListViewHolder.kt
@@ -1,10 +1,11 @@
 package com.heechan.membeder.ui.common.adapter.team
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.heechan.membeder.databinding.RowTeamSelectListItemBinding
 import com.heechan.membeder.ui.SingletonObject
 import com.heechan.membeder.model.data.team.Team
-import com.heechan.membeder.ui.team.manage.TeamSelectActivity
+import com.heechan.membeder.ui.team.select.TeamSelectActivity
 
 class TeamListViewHolder (private val view : RowTeamSelectListItemBinding) : RecyclerView.ViewHolder(view.root) {
     private var position : Int? = null
@@ -12,6 +13,10 @@ class TeamListViewHolder (private val view : RowTeamSelectListItemBinding) : Rec
     fun onBind(teamData : Team, position : Int){
         view.teamData = teamData
         this.position = position
+    }
+
+    constructor(view : RowTeamSelectListItemBinding, clickListener : (View) -> Unit) : this(view){
+        view.root.setOnClickListener(clickListener)
     }
 
     init {
---
+++ app/src/main/java/com/heechan/membeder/ui/team/select/TeamSelectActivity.kt
@@ -0,0 +1,21 @@
+package com.heechan.membeder.ui.team.select
+
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.heechan.membeder.R
+import com.heechan.membeder.base.BaseActivity
+import com.heechan.membeder.databinding.ActivityTeamSelcectTeamBinding
+import com.heechan.membeder.ui.SingletonObject
+import com.heechan.membeder.ui.common.adapter.team.TeamListAdapter
+
+class TeamSelectActivity : BaseActivity<ActivityTeamSelcectTeamBinding>(R.layout.activity_team_selcect_team) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val teamList = SingletonObject.userData.value!!.teamList
+        binding.listTeamSelect.apply {
+            adapter = TeamListAdapter(teamList)
+            layoutManager = LinearLayoutManager(this@TeamSelectActivity)
+        }
+    }
+}
