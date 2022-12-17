4ca7dfba5bd82cba2ecee0a928346dc449cb177a
ryujisung <jiyesung1227@naver.com>
2022-12-18 00:06:01 +0900

    requst

--- app/src/main/java/com/heechan/membeder/ui/team/manage/MemberBanActivity.kt
+++ app/src/main/java/com/heechan/membeder/ui/team/manage/MemberBanActivity.kt
@@ -2,6 +2,7 @@ package com.heechan.membeder.ui.team.manage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heechan.membeder.R
 
 class MemberBanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
