44d5ab700cc4f49ac95a854b936104c123764b3c
ryujisung <jiyesung1227@naver.com>
2022-12-19 23:28:48 +0900

    dod

--- app/src/main/java/com/heechan/membeder/ui/team/caleander/TodoFinalEditActivity.kt
+++ app/src/main/java/com/heechan/membeder/ui/team/caleander/TodoFinalEditActivity.kt
@@ -2,12 +2,21 @@ package com.heechan.membeder.ui.team.caleander
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.fragment.app.activityViewModels
 import com.heechan.membeder.R
 import com.heechan.membeder.base.BaseActivity
 import com.heechan.membeder.databinding.ActivityTodoEditBinding
+import com.heechan.membeder.databinding.ActivityTodoFinalEditBinding
+import com.heechan.membeder.ui.schedule.ScheduleAddActivity
+import com.heechan.membeder.ui.schedule.ScheduleAddViewModel
 
-class TodoFinalEditActivity : BaseActivity<ActivityTodoEditBinding>(R.layout.activity_todo_final_edit) {
+class TodoFinalEditActivity : BaseActivity<ActivityTodoFinalEditBinding>(R.layout.activity_todo_final_edit) {
+    val viewModel : TodoEditViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
+
 }
