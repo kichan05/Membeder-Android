3870869ada24ed47478fbf0fe809dfcf3e19c333
Heechan <ckstmznf@naver.com>
2022-12-21 02:57:25 +0900

    Team logo

--- app/src/main/java/com/heechan/membeder/ui/team/make/TeamMakeViewModel.kt
+++ app/src/main/java/com/heechan/membeder/ui/team/make/TeamMakeViewModel.kt
@@ -2,6 +2,7 @@ package com.heechan.membeder.ui.team.make
 
 import android.app.Application
 import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,16 +10,20 @@ import com.heechan.membeder.model.data.team.Applicant
 import com.heechan.membeder.model.data.team.CreateTeamReq
 import com.heechan.membeder.model.data.team.EditNoticeReq
 import com.heechan.membeder.model.data.team.Team
+import com.heechan.membeder.model.remote.FilePepositoryImpl
 import com.heechan.membeder.model.remote.TeamRepositoryImpl
 import com.heechan.membeder.ui.SingletonObject
 import com.heechan.membeder.ui.common.notifyObserver
+import com.heechan.membeder.util.Default
 import com.heechan.membeder.util.State
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class TeamMakeViewModel(application: Application) : ViewModel() {
+class TeamMakeViewModel(val application: Application) : ViewModel() {
     private val team = TeamRepositoryImpl()
+    private val file = FilePepositoryImpl()
 
     val teamName = MutableLiveData<String>()
     val teamDescription = MutableLiveData<String>()
@@ -32,22 +37,34 @@ class TeamMakeViewModel(application: Application) : ViewModel() {
     val resultData = MutableLiveData<Team>()
     val errorMessage = MutableLiveData<String?>()
 
+    private suspend fun upLoadTeamLodo() : String {
+        val res = file.uploadImage(teamLogo.value!!, application)
+
+        return res.body()!!.path
+    }
+
     fun makeTeam() {
         if(state.value == State.LOADING)
             return
 
         viewModelScope.launch(
-//            CoroutineExceptionHandler { _, e ->
-//            // 에러가 발생 했을때
-//            state.value = State.FAIL
-//            Log.e("[teamMakeError]", e.message.toString())
-//        }
+            CoroutineExceptionHandler { _, e ->
+                // 에러가 발생 했을때
+                state.value = State.FAIL
+                Log.e("[teamMakeError]", e.message.toString())
+            }
         ) {
+            val teamLogoUrl : String = if(teamLogo.value != null) {
+                upLoadTeamLodo()
+            } else {
+                Default.DEFAULT_TEAM_LOGO
+            }
+
             val teamMakeReq = CreateTeamReq(
                 name = teamName.value!!,
                 description = teamDescription.value!!,
                 private = true,
-                image = "image url",
+                image = teamLogoUrl,
                 applicant = Applicant(
                     developer = teamApplicantDeveloper.value?.toIntOrNull() ?: 0,
                     designer = teamApplicantDesigner.value?.toIntOrNull() ?: 0,
